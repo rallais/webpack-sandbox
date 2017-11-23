a66d3b7e3be3b6c043709577a21ec0a537694966
rallais <regis.allais@gmail.com>
2017-11-23 17:28:49 +0100

    extend devServer options in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const path = require('path');
-const dist = path.resolve(__dirname, 'dist');
 
+const dist = path.resolve(__dirname, 'dist');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackManifestPlugin = require('webpack-manifest-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
 
 const config = {
     entry: {
@@ -17,6 +20,7 @@ const config = {
         path: dist
     },
     plugins: [
+        new HotModuleReplacementPlugin(),
         new WebpackManifestPlugin(),
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
@@ -26,6 +30,9 @@ const config = {
         }),
         new UglifyJsPlugin({
             sourceMap: true
+        }),
+        new DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
         })
     ],
     module: {
@@ -40,11 +47,12 @@ const config = {
     ,
     devtool: 'source-map',
     devServer: {
+        host: '0.0.0.0',
         port: 9000,
-        contentBase:
-        dist,
-        openPage:
-            'app.html'
+        contentBase: dist,
+        openPage: 'app.html',
+        hot: true,
+        open: true
     }
 };
 
